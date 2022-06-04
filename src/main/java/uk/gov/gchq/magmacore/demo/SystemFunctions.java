fae65f8893a489118b6106a749054fb2c439fa86
Tony Walmsley <tony@aosd.co.uk>
2022-06-04 15:58:16 +0100

    Improvements to the new example code.

--- src/main/java/uk/gov/gchq/magmacore/demo/SystemFunctions.java
+++ src/main/java/uk/gov/gchq/magmacore/demo/SystemFunctions.java
@@ -1,24 +1,16 @@
 package uk.gov.gchq.magmacore.demo;
 
-import java.util.function.Function;
 import java.util.function.Supplier;
+import java.util.function.UnaryOperator;
 
 import uk.gov.gchq.hqdm.iri.HQDM;
-import uk.gov.gchq.hqdm.model.Association;
 import uk.gov.gchq.hqdm.model.ClassOfStateOfFunctionalSystem;
 import uk.gov.gchq.hqdm.model.ClassOfStateOfPerson;
-import uk.gov.gchq.hqdm.model.Event;
-import uk.gov.gchq.hqdm.model.FunctionalSystem;
 import uk.gov.gchq.hqdm.model.KindOfAssociation;
-import uk.gov.gchq.hqdm.model.KindOfBiologicalSystemComponent;
 import uk.gov.gchq.hqdm.model.KindOfFunctionalSystem;
-import uk.gov.gchq.hqdm.model.KindOfFunctionalSystemComponent;
 import uk.gov.gchq.hqdm.model.KindOfPerson;
-import uk.gov.gchq.hqdm.model.Participant;
-import uk.gov.gchq.hqdm.model.Person;
 import uk.gov.gchq.hqdm.model.Role;
-import uk.gov.gchq.hqdm.model.StateOfFunctionalSystem;
-import uk.gov.gchq.hqdm.model.StateOfPerson;
+import uk.gov.gchq.hqdm.model.Thing;
 import uk.gov.gchq.magmacore.database.MagmaCoreDatabase;
 import uk.gov.gchq.magmacore.database.MagmaCoreObjectDatabase;
 
@@ -31,69 +23,69 @@ public class SystemFunctions {
     //  A Supplier of a MagmaCoreDatabase.
     private static Supplier<MagmaCoreDatabase> createDatabase = () -> new MagmaCoreObjectDatabase();
 
-    // A Supplier of a ModelBuilder.
-    private static Supplier<ModelBuilder> createRefDataObjects = () -> {
-        final ModelBuilder builder = new ModelBuilder();
+    // A DB transformer that adds the RDL entities.
+    private static UnaryOperator<MagmaCoreDatabase> createRefDataObjects = (db) -> {
+        final var builder = new ModelBuilder();
 
         // RDL CLASSES - Can be created, stored and queried separately.
 
         // Viewable is a class to assign other data objects to, to indicate that they are likely to
         // be of direct interest to a system user.
-        final uk.gov.gchq.hqdm.model.Class viewable = builder.createClass("VIEWABLE");
+        final var viewable = builder.createClass("VIEWABLE");
 
         // A sub-set of the Viewable class.
-        final uk.gov.gchq.hqdm.model.Class viewableObject = builder.createClass("VIEWABLE_OBJECT");
+        final var viewableObject = builder.createClass("VIEWABLE_OBJECT");
 
         // A sub-set of the Viewable Class for viewable Associations.
-        final uk.gov.gchq.hqdm.model.Class viewableAssociation = builder.createClass("VIEWABLE_ASSOCIATION");
+        final var viewableAssociation = builder.createClass("VIEWABLE_ASSOCIATION");
 
         // A system is composed of components so this is the class of components that a whole-life
         // person can have.
-        final KindOfBiologicalSystemComponent kindOfBiologicalSystemHumanComponent =
+        final var kindOfBiologicalSystemHumanComponent =
             builder.createKindOfBiologicalSystemComponent("KIND_OF_BIOLOGICAL_SYSTEM_HUMAN_COMPONENT");
 
         // A class of whole-life person (re-)created as Reference Data.
-        final KindOfPerson kindOfPerson = builder.createKindOfPerson("KIND_OF_PERSON");
+        final var kindOfPerson = builder.createKindOfPerson("KIND_OF_PERSON");
 
         // A class of temporal part (state) of a (whole-life) person.
-        final ClassOfStateOfPerson classOfStateOfPerson = 
+        final var classOfStateOfPerson =
             builder.createClassOfStateOfPerson("CLASS_OF_STATE_OF_PERSON");
 
         // A class of whole-life system that is a Building.
-        final KindOfFunctionalSystem kindOfFunctionalSystemBuilding =
+        final var kindOfFunctionalSystemBuilding =
             builder.createKindOfFunctionalSystem("KIND_OF_FUNCTIONAL_SYSTEM_BUILDING");
 
         // A Domestic Property is a system composed of components (e.g. walls, floors, roof, front
         // door, etc). This is the class of those whole-life system components.
-        final KindOfFunctionalSystemComponent kindOfFunctionalSystemDomesticPropertyComponent =
+        final var kindOfFunctionalSystemDomesticPropertyComponent =
             builder.createKindOfFunctionalSystemComponent("KIND_OF_FUNCTIONAL_SYSTEM_DOMESTIC_PROPERTY_COMPONENT");
 
         // The class of whole-life system that is domestic property.
-        final KindOfFunctionalSystem kindOfFunctionalSystemDomesticProperty =
+        final var kindOfFunctionalSystemDomesticProperty =
             builder.createKindOfFunctionalSystem("KIND_OF_FUNCTIONAL_SYSTEM_DOMESTIC_PROPERTY");
 
         // The class of state of system whose members are temporal parts of domestic properties.
-        final ClassOfStateOfFunctionalSystem classOfStateOfFunctionalSystemDomesticProperty =
+        final var classOfStateOfFunctionalSystemDomesticProperty =
             builder.createClassOfStateOfFunctionalSystem("STATE_OF_FUNCTIONAL_SYSTEM_DOMESTIC_PROPERTY");
 
         // The class of role that every member of class of person plays.
-        final Role personRole = builder.createRole("NATURAL_MEMBER_OF_SOCIETY_ROLE");
+        final var personRole = builder.createRole("NATURAL_MEMBER_OF_SOCIETY_ROLE");
 
         // The class of role that every member of class of domestic property plays.
-        final Role domesticPropertyRole = builder.createRole("ACCEPTED_PLACE_OF_SEMI_PERMANENT_HABITATION_ROLE");
+        final var domesticPropertyRole = builder.createRole("ACCEPTED_PLACE_OF_SEMI_PERMANENT_HABITATION_ROLE");
 
-        final Role domesticOccupantInPropertyRole = builder.createRole("DOMESTIC_PROPERTY_THAT_IS_OCCUPIED_ROLE");
+        final var domesticOccupantInPropertyRole = builder.createRole("DOMESTIC_PROPERTY_THAT_IS_OCCUPIED_ROLE");
         // Would be good to add part_of_by_class_(occupantInPropertyKindOfAssociation) but can't
         // neatly do that in the class as it can only be added after
         // occupantInPropertyKindOfAssociation is created. This can be added later for completeness.
 
-        final Role occupierOfPropertyRole = builder.createRole("OCCUPIER_LOCATED_IN_PROPERTY_ROLE");
+        final var occupierOfPropertyRole = builder.createRole("OCCUPIER_LOCATED_IN_PROPERTY_ROLE");
         // Would be good to add part_of_by_class_(occupantInPropertyKindOfAssociation) but can't
         // neatly do that in the class as it can only be added after
         // occupantInPropertyKindOfAssociation is created. This can be added later for completeness.
 
         // Add the Association Types (Participants and Associations).
-        final KindOfAssociation occupantInPropertyKindOfAssociation =
+        final var occupantInPropertyKindOfAssociation =
             builder.createKindOfAssociation("OCCUPANT_LOCATED_IN_VOLUME_ENCLOSED_BY_PROPERTY_ASSOCIATION");
 
         builder.addSubclass(viewable, viewableObject);
@@ -109,74 +101,95 @@ public class SystemFunctions {
         builder.addClassMember(viewableAssociation, occupantInPropertyKindOfAssociation);
 
         builder.addHasComponentByClass(kindOfPerson, kindOfBiologicalSystemHumanComponent);
-        builder.addHasComponentByClass(kindOfFunctionalSystemDomesticProperty, 
+        builder.addHasComponentByClass(kindOfFunctionalSystemDomesticProperty,
                 kindOfFunctionalSystemDomesticPropertyComponent);
 
         builder.addConsistsOfByClass(occupantInPropertyKindOfAssociation, domesticOccupantInPropertyRole);
         builder.addConsistsOfByClass(occupantInPropertyKindOfAssociation, occupierOfPropertyRole);
 
-        return builder;
+        builder.getObjects().forEach(object -> {
+            db.create(object);
+        });
+
+        return db;
     };
 
+
+    /**
+     * Find an object by its ENTITY_NAME.
+     *
+     * @param db the {@link MagmaCoreDatabase} to seaerch.
+     * @param name the name {@link String} to seaerch for.
+     * @return the {@link Thing}that was found.
+     * @throws RuntimeException if no or multiple results found.
+    */
+    private static Thing findByEntityName(final MagmaCoreDatabase db, final String name) {
+        final var searchResult = db.findByPredicateIriAndStringValue(HQDM.ENTITY_NAME, name);
+        if (searchResult.size() == 1) {
+            return searchResult.get(0);
+        } else if (searchResult.size() == 0) {
+            throw new RuntimeException("No entity found with name: " + name);
+        } else {
+            throw new RuntimeException("Multiple entities found with name: " + name);
+        }
+    }
+
     // A DB transformer that adds example entity instances.
-    private static Function<MagmaCoreDatabase, MagmaCoreDatabase> addExampleEntities = (db) -> {
+    private static UnaryOperator<MagmaCoreDatabase> addExampleEntities = (db) -> {
+
+        // Find the required classes, kinds, and roles.
         final var kindOfPerson = (KindOfPerson) 
-            db.findByPredicateIriAndStringValue(HQDM.ENTITY_NAME, "KIND_OF_PERSON").get(0);
+            findByEntityName(db, "KIND_OF_PERSON");
         final var personRole = (Role) 
-            db.findByPredicateIriAndStringValue(HQDM.ENTITY_NAME, "NATURAL_MEMBER_OF_SOCIETY_ROLE").get(0);
+            findByEntityName(db, "NATURAL_MEMBER_OF_SOCIETY_ROLE");
         final var classOfStateOfPerson = (ClassOfStateOfPerson) 
-            db.findByPredicateIriAndStringValue(HQDM.ENTITY_NAME, "CLASS_OF_STATE_OF_PERSON").get(0);
+            findByEntityName(db, "CLASS_OF_STATE_OF_PERSON");
         final var kindOfFunctionalSystemDomesticProperty = (KindOfFunctionalSystem) 
-            db.findByPredicateIriAndStringValue(HQDM.ENTITY_NAME, "KIND_OF_FUNCTIONAL_SYSTEM_DOMESTIC_PROPERTY").get(0);
+            findByEntityName(db, "KIND_OF_FUNCTIONAL_SYSTEM_DOMESTIC_PROPERTY");
         final var domesticPropertyRole = (Role) 
-            db.findByPredicateIriAndStringValue(HQDM.ENTITY_NAME, 
-                    "ACCEPTED_PLACE_OF_SEMI_PERMANENT_HABITATION_ROLE").get(0);
+            findByEntityName(db, "ACCEPTED_PLACE_OF_SEMI_PERMANENT_HABITATION_ROLE");
         final var classOfStateOfFunctionalSystemDomesticProperty = (ClassOfStateOfFunctionalSystem) 
-            db.findByPredicateIriAndStringValue(HQDM.ENTITY_NAME,
-                    "STATE_OF_FUNCTIONAL_SYSTEM_DOMESTIC_PROPERTY").get(0);
+            findByEntityName(db, "STATE_OF_FUNCTIONAL_SYSTEM_DOMESTIC_PROPERTY");
         final var occupierOfPropertyRole = (Role) 
-            db.findByPredicateIriAndStringValue(HQDM.ENTITY_NAME,
-                    "OCCUPIER_LOCATED_IN_PROPERTY_ROLE").get(0);
+            findByEntityName(db, "OCCUPIER_LOCATED_IN_PROPERTY_ROLE");
         final var domesticOccupantInPropertyRole = (Role) 
-            db.findByPredicateIriAndStringValue(HQDM.ENTITY_NAME,
-                    "DOMESTIC_PROPERTY_THAT_IS_OCCUPIED_ROLE").get(0);
-        final var occupantInPropertyKindOfAssociation = (KindOfAssociation)
-            db.findByPredicateIriAndStringValue(HQDM.ENTITY_NAME,
-                    "OCCUPANT_LOCATED_IN_VOLUME_ENCLOSED_BY_PROPERTY_ASSOCIATION").get(0);
+            findByEntityName(db, "DOMESTIC_PROPERTY_THAT_IS_OCCUPIED_ROLE");
+        final var occupantInPropertyKindOfAssociation = (KindOfAssociation) 
+            findByEntityName(db, "OCCUPANT_LOCATED_IN_VOLUME_ENCLOSED_BY_PROPERTY_ASSOCIATION");
 
         // STATES
 
         // The main state: This is a mandatory component of all datasets if we are to stick to the
         // commitments in HQDM. This is the least strict treatment, the creation of a single
         // possible world.
-        final PossibleWorldContext pwContext = new PossibleWorldContext("Example1_World");
+        final var pwContext = new PossibleWorldContext("Example1_World");
 
         // Person B Whole Life Object.
-        final Event e1 = pwContext.createPointInTime("1991-02-18T00:00:00");
+        final var e1 = pwContext.createPointInTime("1991-02-18T00:00:00");
 
-        final Person personB1 = pwContext.createPerson("PersonB1_Bob");
+        final var personB1 = pwContext.createPerson("PersonB1_Bob");
 
         pwContext.addMemberOfKind(personB1, kindOfPerson);
         pwContext.addNaturalRole(personB1, personRole);
         pwContext.addBeginningEvent(personB1, e1);
 
         // Person B states.
-        final Event e2 = pwContext.createPointInTime("2020-08-15T17:50:00");
-        final Event e3 = pwContext.createPointInTime("2020-08-15T19:21:00");
+        final var e2 = pwContext.createPointInTime("2020-08-15T17:50:00");
+        final var e3 = pwContext.createPointInTime("2020-08-15T19:21:00");
 
 
-        final StateOfPerson personBs1 = pwContext.createStateOfPerson("");
+        final var personBs1 = pwContext.createStateOfPerson("");
 
         pwContext.addMemberOf(personBs1, classOfStateOfPerson);
         pwContext.addTemporalPartOf(personBs1, personB1);
         pwContext.addBeginningEvent(personBs1, e2);
         pwContext.addEndingEvent(personBs1, e3);
 
-        final Event e4 = pwContext.createPointInTime("2020-08-16T22:33:00");
-        final Event e5 = pwContext.createPointInTime("2020-08-17T10:46:00");
+        final var e4 = pwContext.createPointInTime("2020-08-16T22:33:00");
+        final var e5 = pwContext.createPointInTime("2020-08-17T10:46:00");
 
 
-        final StateOfPerson personBs2 = pwContext.createStateOfPerson("");
+        final var personBs2 = pwContext.createStateOfPerson("");
 
         pwContext.addMemberOf(personBs2, classOfStateOfPerson);
         pwContext.addTemporalPartOf(personBs2, personB1);
@@ -185,22 +198,22 @@ public class SystemFunctions {
 
 
         // House B Whole Life Object.
-        final Event e6 = pwContext.createPointInTime("1972-06-01T00:00:00");
-        final FunctionalSystem houseB = pwContext.createFunctionalSystem("");
+        final var e6 = pwContext.createPointInTime("1972-06-01T00:00:00");
+        final var houseB = pwContext.createFunctionalSystem("");
 
         pwContext.addMemberOfKind(houseB, kindOfFunctionalSystemDomesticProperty);
         pwContext.addIntendedRole(houseB, domesticPropertyRole);
         pwContext.addBeginningEvent(houseB, e6);
 
         // States of house when Occupant personBs1 is present.
-        final StateOfFunctionalSystem houseBs1 = pwContext.createStateOfFunctionalSystem("");
+        final var houseBs1 = pwContext.createStateOfFunctionalSystem("");
 
         pwContext.addMemberOf(houseBs1, classOfStateOfFunctionalSystemDomesticProperty);
         pwContext.addTemporalPartOf(houseB, houseBs1);
         pwContext.addBeginningEvent(houseBs1, e2);
         pwContext.addEndingEvent(houseBs1, e3);
 
-        final StateOfFunctionalSystem houseBs2 = pwContext.createStateOfFunctionalSystem("");;
+        final var houseBs2 = pwContext.createStateOfFunctionalSystem("");;
 
         pwContext.addMemberOf(houseBs2, classOfStateOfFunctionalSystemDomesticProperty);
         pwContext.addTemporalPartOf(houseB, houseBs2);
@@ -211,7 +224,7 @@ public class SystemFunctions {
         // If we had full has_superClass resolving in HQDM classes then this participant object
         // wouldn't be needed as the class occupierOfPropertyRole is also a sub-type of
         // state_of_person (see issues list).
-        final Participant pPersonBs1 =
+        final var pPersonBs1 =
             pwContext.createParticipant("Note this is the state of person Bs1 that is participating the association");
 
         pwContext.addMemberOfKind(pPersonBs1, occupierOfPropertyRole);
@@ -219,7 +232,7 @@ public class SystemFunctions {
         pwContext.addBeginningEvent(pPersonBs1, e2);
         pwContext.addEndingEvent(pPersonBs1, e3);
 
-        final Participant pHouseBs1 =
+        final var pHouseBs1 =
             pwContext.createParticipant("Note this is the state of houseBs1 that is participating in the association");
 
         pwContext.addMemberOfKind(pHouseBs1, domesticOccupantInPropertyRole);
@@ -227,7 +240,7 @@ public class SystemFunctions {
         pwContext.addBeginningEvent(pHouseBs1, e2);
         pwContext.addEndingEvent(pHouseBs1, e3);
 
-        final Participant pPersonBs2 =
+        final var pPersonBs2 =
           pwContext.createParticipant("Note this is the state of person Bs2 that is participating in the association");
 
         pwContext.addMemberOfKind(pPersonBs2, occupierOfPropertyRole);
@@ -235,7 +248,7 @@ public class SystemFunctions {
         pwContext.addBeginningEvent(pPersonBs2, e4);
         pwContext.addEndingEvent(pPersonBs2, e5);
 
-        final Participant pHouseBs2 =
+        final var pHouseBs2 =
             pwContext.createParticipant("Note this is the state of houseBs2 that is participating in the association");
 
         pwContext.addMemberOfKind(pHouseBs2, domesticOccupantInPropertyRole);
@@ -243,7 +256,7 @@ public class SystemFunctions {
         pwContext.addBeginningEvent(pHouseBs2, e4);
         pwContext.addEndingEvent(pHouseBs2, e5);
 
-        final Association houseOccupantPresentState1 = pwContext.createAssociation("HouseOccupantPresent1");
+        final var houseOccupantPresentState1 = pwContext.createAssociation("HouseOccupantPresent1");
 
         pwContext.addMemberOfKind(houseOccupantPresentState1, occupantInPropertyKindOfAssociation);
         pwContext.addConsistsOfParticipant(houseOccupantPresentState1, pHouseBs1);
@@ -251,7 +264,7 @@ public class SystemFunctions {
         pwContext.addBeginningEvent(houseOccupantPresentState1, e2);
         pwContext.addEndingEvent(houseOccupantPresentState1, e3);
 
-        final Association houseOccupantPresentState2 = pwContext.createAssociation("HouseOccupantPresent2");
+        final var houseOccupantPresentState2 = pwContext.createAssociation("HouseOccupantPresent2");
 
         pwContext.addMemberOfKind(houseOccupantPresentState2, occupantInPropertyKindOfAssociation);
         pwContext.addConsistsOfParticipant(houseOccupantPresentState2, pHouseBs2);
@@ -259,13 +272,7 @@ public class SystemFunctions {
         pwContext.addBeginningEvent(houseOccupantPresentState2, e4);
         pwContext.addEndingEvent(houseOccupantPresentState2, e5);
 
-        return db;
-    };
-
-    // A DB transformer that adds Reference Data entities.
-    private static Function<MagmaCoreDatabase, MagmaCoreDatabase> addReferenceDataLibrary = (db) -> {
-
-        createRefDataObjects.get().getObjects().forEach(object -> {
+        pwContext.getObjects().forEach(object -> {
             db.create(object);
         });
 
@@ -275,8 +282,8 @@ public class SystemFunctions {
     // A Runnable that creates a database and populates it.
     public static Runnable runner = () -> {
 
-        final Function<MagmaCoreDatabase, MagmaCoreDatabase> runDbOperations =
-            addReferenceDataLibrary
+        final var runDbOperations =
+            createRefDataObjects
             .andThen(addExampleEntities);
 
         runDbOperations.apply(createDatabase.get());
