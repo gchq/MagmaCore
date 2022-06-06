2bec6272bf9dc31a146af133df8f412697d7fdf4
Tony Walmsley <tony@aosd.co.uk>
2022-06-06 13:33:21 +0100

    Don't register REF_BASE and USER_BASE with Jena.

--- src/main/java/uk/gov/gchq/magmacore/demo/JenaDatabaseDemo.java
+++ src/main/java/uk/gov/gchq/magmacore/demo/JenaDatabaseDemo.java
@@ -17,8 +17,6 @@ package uk.gov.gchq.magmacore.demo;
 import static uk.gov.gchq.hqdm.iri.HQDM.ENTITY_NAME;
 import static uk.gov.gchq.hqdm.iri.HQDM.HQDM;
 import static uk.gov.gchq.hqdm.iri.RDFS.RDFS;
-import static uk.gov.gchq.magmacore.util.DataObjectUtils.REF_BASE;
-import static uk.gov.gchq.magmacore.util.DataObjectUtils.USER_BASE;
 
 import java.util.List;
 
@@ -55,8 +53,6 @@ public final class JenaDatabaseDemo {
         final MagmaCoreJenaDatabase jenaDatabase = new MagmaCoreJenaDatabase();
         jenaDatabase.register(HQDM);
         jenaDatabase.register(RDFS);
-        jenaDatabase.register(REF_BASE);
-        jenaDatabase.register(USER_BASE);
 
         // Add example data objects to dataset.
         jenaDatabase.begin();
