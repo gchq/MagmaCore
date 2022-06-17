de8d425a6af086cf0c3b91505cd43d840b5a06af
Tony Walmsley <tony@aosd.co.uk>
2022-06-17 14:31:16 +0100

    Rename class

--- src/test/java/uk/gov/gchq/magmacore/demo/ExampleSignsTest.java
+++ src/test/java/uk/gov/gchq/magmacore/demo/ExampleSignsTest.java
@@ -10,14 +10,14 @@ import uk.gov.gchq.magmacore.service.MagmaCoreServiceFactory;
  * Exercise the {@link ExampleSigns} code during the build.
  *
  * */
-public class ExampleiSignsTest {
+public class ExampleSignsTest {
 
     /**
      * Test the {@link ExampleSigns} code.
      *
      * */
     @Test
-    public void testApplyAndInvert() {
+    public void testSignsExample() {
         final var service = MagmaCoreServiceFactory.createWithJenaDatabase();
         
         final var transformation = ExampleSigns.populateExampleData(service);
