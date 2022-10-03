38130b13579ed4ea1a84199f75bca0e8ea757b59
Benjamin 59015 <70384549+GCHQDeveloper42@users.noreply.github.com>
2022-10-03 16:01:01 +0000

    Removed ENTITY_ID from HQDM RDF definitions.

--- hqdm-rdf/src/main/java/uk/gov/gchq/magmacore/hqdm/rdf/iri/HQDM.java
+++ hqdm-rdf/src/main/java/uk/gov/gchq/magmacore/hqdm/rdf/iri/HQDM.java
@@ -25,10 +25,6 @@ public final class HQDM {
     /** HQDM namespace. */
     public static final IriBase HQDM = new IriBase("hqdm", "http://www.semanticweb.org/hqdm#");
 
-    /** A unique identifier for a particular HQDM entity. */
-    @Deprecated
-    public static final HqdmIri ENTITY_ID = new HqdmIri(HQDM, "data_uniqueID");
-
     /** A human-interpretable name for a particular HQDM entity. */
     public static final HqdmIri ENTITY_NAME = new HqdmIri(HQDM, "data_EntityName");
 
