885412681bf4949937026eecd9ab63f9b0ad111b
Tony <tony@aosd.co.uk>
2024-02-08 11:20:11 +0000

    Checkpoint - added some validation rules. Not finished yet.

--- examples/src/test/java/uk/gov/gchq/magmacore/examples/verify/DataIntegrityChecksTest.java
+++ examples/src/test/java/uk/gov/gchq/magmacore/examples/verify/DataIntegrityChecksTest.java
@@ -12,10 +12,17 @@ import java.util.UUID;
 import org.junit.Test;
 
 import uk.gov.gchq.magmacore.database.validation.ValidationReportEntry;
+import uk.gov.gchq.magmacore.hqdm.model.Organization;
+import uk.gov.gchq.magmacore.hqdm.model.Person;
+import uk.gov.gchq.magmacore.hqdm.model.PossibleWorld;
+import uk.gov.gchq.magmacore.hqdm.model.Role;
+import uk.gov.gchq.magmacore.hqdm.model.Sign;
 import uk.gov.gchq.magmacore.hqdm.model.Thing;
+import uk.gov.gchq.magmacore.hqdm.rdf.iri.HQDM;
 import uk.gov.gchq.magmacore.hqdm.rdf.iri.IRI;
 import uk.gov.gchq.magmacore.hqdm.rdf.iri.IriBase;
 import uk.gov.gchq.magmacore.hqdm.services.ClassServices;
+import uk.gov.gchq.magmacore.hqdm.services.SpatioTemporalExtentServices;
 import uk.gov.gchq.magmacore.service.MagmaCoreService;
 import uk.gov.gchq.magmacore.service.MagmaCoreServiceFactory;
 
@@ -44,10 +51,39 @@ public class DataIntegrityChecksTest {
 
         // Populate some data to prove that the rules are applied.
         service.runInWriteTransaction(svc -> {
-
             // Create a kind without an ENTITY_NAME.
-            final Thing kind = ClassServices.createKindOfOrganization(new IRI(TEST_BASE, UUID.randomUUID().toString()));
+            final Thing kind = ClassServices.createKindOfOrganization(randomIri());
             svc.create(kind);
+
+            // Create a participant that isn't a member_of_kind of a role.
+            // Also, as a SpatioTemporalExtent it should have a part_of_possible_world predicate.
+            final Thing participant = SpatioTemporalExtentServices.createParticipant(randomIri());
+            svc.create(participant);
+
+            // Create an entity with invalid temporal parthood.
+            final PossibleWorld possibleWorld = SpatioTemporalExtentServices.createPossibleWorld(randomIri());
+            possibleWorld.addValue(HQDM.PART_OF_POSSIBLE_WORLD, possibleWorld.getId());
+
+            final Person person = SpatioTemporalExtentServices.createPerson(randomIri());
+            person.addValue(HQDM.PART_OF_POSSIBLE_WORLD, possibleWorld.getId());
+
+            final Organization organization = SpatioTemporalExtentServices.createOrganization(randomIri());
+            organization.addValue(HQDM.PART_OF_POSSIBLE_WORLD, possibleWorld.getId());
+
+            person.addValue(HQDM.TEMPORAL_PART_OF, organization.getId());
+
+            svc.create(possibleWorld);
+            svc.create(person);
+            svc.create(organization);
+
+            // Create a sign without a value_ predicate.
+            final Sign sign = SpatioTemporalExtentServices.createSign(randomIri());
+            sign.addValue(HQDM.PART_OF_POSSIBLE_WORLD, possibleWorld.getId());
+            final Role signRole = ClassServices.createRole(randomIri());
+            signRole.addValue(HQDM.ENTITY_NAME, "A role for a sign");
+            sign.addValue(HQDM.MEMBER_OF_KIND, signRole.getId());
+            svc.create(sign);
+            svc.create(signRole);
             return svc;
         });
 
@@ -61,7 +97,15 @@ public class DataIntegrityChecksTest {
         if (validationResult.size() > 0) {
             System.out.println(validationResult);
         }
-        assertEquals(1, validationResult.size());
+        assertEquals(5, validationResult.size());
     }
 
+    /**
+     * Make random IRIs.
+     *
+     * @return a random {@link IRI}
+     */
+    private static IRI randomIri() {
+        return new IRI(TEST_BASE, UUID.randomUUID().toString());
+    }
 }
