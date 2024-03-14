295e065bb1189352c0f7815a1204e78e6325334b
Tony <tony@aosd.co.uk>
2024-03-14 16:07:42 +0000

    Add useful comments to explain what is going on in a unit test.

--- examples/src/test/java/uk/gov/gchq/magmacore/examples/kinds/CreateOrganizationSubTypeUsingKinds.java
+++ examples/src/test/java/uk/gov/gchq/magmacore/examples/kinds/CreateOrganizationSubTypeUsingKinds.java
@@ -24,39 +24,62 @@ import uk.gov.gchq.magmacore.service.MagmaCoreServiceFactory;
 /**
  * Create a KindOfOrganization and make a Thing a member_of_kind of it,
  * then persist the Thing and make sure it is read back as an Organization.
+ *
+ * This demonstrates the implementation of instances of 'kinds' as new entity types.
  */
 public class CreateOrganizationSubTypeUsingKinds {
+    /*
+     * A Base URL for the tests.
+     */
     private static IriBase TEST_BASE = new IriBase("test", "http://example.com/test#");
     
+    /**
+     * Unit test.
+     */
     @Test
     public void test() {
+        // ACME Ltd will be a part of this possibleWorld.
         final PossibleWorld possibleWorld = SpatioTemporalExtentServices.createPossibleWorld(randomIri());
+
+        // We need a kind of organization component for the kind of organization.
         final KindOfOrganizationComponent componentKind = new KindOfOrganizationComponentBuilder(randomIri())
             .build();
 
+        // Create a new kind of organization to represent companies.
+        // This will also make the new kind into a sub-type of organization.
         final KindOfOrganization companies = new KindOfOrganizationBuilder(randomIri())
             .has_Component_By_Class_M(componentKind)
             .build(); 
 
+        // Create an Individual and make it a member_of_kind of the companies kind.
+        // This will also mean that the individual is  of type organization, via its kind.
         final Individual acmeLtd = new IndividualBuilder(randomIri())
             .member_Of_Kind(companies)
             .part_Of_Possible_World_M(possibleWorld)
             .build();
 
+        // Persist everything.
         final MagmaCoreService service = MagmaCoreServiceFactory.createWithJenaDatabase();
         service.beginWrite();
         service.create(companies);
         service.create(acmeLtd);
         service.commit();
 
+        // Read back the organization.
         service.beginRead();
         final Thing acmeFromDatabase = service.get(acmeLtd.getId());
         service.commit();
 
+        // Confirm that the entity created as an individual is now actually an organization as well.
         assertTrue(acmeFromDatabase instanceof Organization);
     }
 
-    private IRI randomIri() {
+    /**
+     * Utility method.
+     *
+     * @return IRI
+     */
+    private static  IRI randomIri() {
         return new IRI(TEST_BASE, UUID.randomUUID().toString());
     }
 }
